864227af1fdb3c6f8a2813ede7f306f1a0f5d09c
thebethbott <bebasile@gmail.com>
2019-02-21 18:24:13 -0500

    taking out parse float

--- homework/projectData.js
+++ homework/projectData.js
@@ -5,6 +5,6 @@ d3.csv("scotusdatabyissue2.16.19.csv", function(error,data) {
     data.forEach(function(d) {
         console.log(d);
             //parseFloat takes csv numbers and turns them into fuctional numbers
-            d.export = parseFloat(d.export);
+            //d.export = parseFloat(d.export);
     });
 });
