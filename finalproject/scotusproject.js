8689c32380b0a7f168ba65b51fdd604a259ff363
thebethbott <bebasile@gmail.com>
2019-02-27 21:35:54 -0500

    Establishing files and intro

---
+++ finalproject/scotusproject.js
@@ -0,0 +1,10 @@
+
+d3.csv("scotusdatabyissue2.16.19.csv", function(error,data) {
+    console.log("error:", error);
+    console.log("csv:", data);
+    data.forEach(function(d) {
+        console.log(d);
+            //parseFloat takes csv numbers and turns them into fuctional numbers
+            //d.export = parseFloat(d.export);
+    });
+});
