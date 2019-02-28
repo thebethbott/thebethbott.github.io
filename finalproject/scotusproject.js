fce1956c71e8f4ffa0f35a3d575f8cb2d3630ed3
thebethbott <bebasile@gmail.com>
2019-02-27 21:36:04 -0500

    cont.

--- finalproject/scotusproject.js
+++ finalproject/scotusproject.js
@@ -4,7 +4,5 @@ d3.csv("scotusdatabyissue2.16.19.csv", function(error,data) {
     console.log("csv:", data);
     data.forEach(function(d) {
         console.log(d);
-            //parseFloat takes csv numbers and turns them into fuctional numbers
-            //d.export = parseFloat(d.export);
     });
 });
