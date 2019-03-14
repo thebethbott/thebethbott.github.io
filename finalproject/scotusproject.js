6b9033eb8a72ff2a0b1015c8ed3e37eb95682063
thebethbott <bebasile@gmail.com>
2019-03-13 20:55:50 -0400

    Yikes first pass

--- finalproject/scotusproject.js
+++ finalproject/scotusproject.js
@@ -1,8 +1,25 @@
+d3.csv("scotusdatabyissue2.16.19.csv", data.forEach (function issueFrquency (d) {
+    var groupIssue = d3.nest()
+    .key(function(d) { return d.caseIssuesId; })
+    .rollup(function(v) { return v.frequency; })
+    .entries(issues);
 
-d3.csv("scotusdatabyissue2.16.19.csv", function(error,data) {
-    console.log("error:", error);
-    console.log("csv:", data);
-    data.forEach(function(d) {
-        console.log(d);
+    var svg = d3.select("vizOne")  
+    var rectIssueFrequency = svg.selectAll("rect")
+        .data(groupIssue);
+
+    var groupIssueBarEnter = rectIssueFrequency.enter().append("rect");
+
+    groupIssueBarEnter
+    .attr("class", "rect")
+    .attr("fill", "black")
+    .attr("y", 70)
+    .attr("width", 10)
+    .attr(function(d) {
+        return d.rectIssueFrequency / 100;
+    })
+    .attr("x", function(d, i) { 
+        return i * 100 + 30; 
     });
+    
 });
