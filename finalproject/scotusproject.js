bc980caefc75552330c1f5c8df6d9eda1b1bac07
thebethbott <bebasile@gmail.com>
2019-03-28 12:21:16 -0400

    text update, axis trials, and label trials

--- finalproject/scotusproject.js
+++ finalproject/scotusproject.js
@@ -1,12 +1,54 @@
+//Line Enter on D3 Attempt
+//var starterData = [1,2,3,4,5,6,7,8,9];
+//
+//        var svg = d3.select("svg");
+
+//        var line = svg.selectAll("line")
+//            .data(starterData);
+//
+//        line.enter().append("line")
+//            .attr("class", "line")
+//            .attr("stroke", "grey")
+//            .attr("stroke-width", "1.25")
+//            .attr("y1", "80")
+//            .attr("y2", "490")
+//            .attr("x1", function(d.starterData) {
+//            return i * 100 + 40;
+//            })
+//            .attr("x2", function(d,i) {
+//            return i * 100 + 40;
+//            });
 
 
 d3.csv("scotusdatabyissue2.16.19.csv", function(error, data) {
     var groupIssue = d3.nest()
         .key(function(d) { return d.issueArea; })
-        // .rollup(function(v) { return v.frequency; })
         .entries(data);
 
+    //X-axis Attempt 1
+    //var x_domain = d3.extent(data, function(d) { return d.key; }),
+        
+    //var xScale = d3.values.scaleLinear()
+        //.domain(x_domain) 
+        //.range([200, 1000]);
+    
     var svg = d3.select("#vizOne") 
+
+    //X-Axis Attempt 2 
+    //var xScale = d3.scaleLinear()
+            //.domain([0, d3.max(d3.values(groupIssue))])
+            //.range([200, 1000]);
+    
+    //var axis = d3.axisBottom(xScale);
+        //d3.select("#xAxis").call(axis);
+
+    //X-Axis Attempt 3
+    //var xScale = d3.scaleLinear()
+            //.domain([0, 2900)])
+            //.range([200, 1000]);
+    
+    //var axis = d3.axisBottom(xScale);
+        //d3.select("#xAxis").call(axis);
 
     var rectIssueFrequency = svg.selectAll("rect")
         .data(groupIssue);
@@ -14,39 +56,42 @@ d3.csv("scotusdatabyissue2.16.19.csv", function(error, data) {
     var groupIssueBarEnter = rectIssueFrequency.enter().append("rect");
 
     groupIssueBarEnter
-    .attr("class", "rect")
-    .attr("fill", "slategrey")
-    .attr("stroke", "darkslategrey")
-    .attr("stroke-width", "1.25")
-    .attr("x", "202")
-    .attr("height", 13)
-    .attr("width", function(d) {
-        console.log(d)
-        return d.values.length / 3;
-    })
-    .attr("y", function(d, i) { 
-        return i * 28 + 90; 
-    });
+        .attr("class", "rect")
+        .attr("fill", "slategrey")
+        .attr("stroke", "darkslategrey")
+        .attr("stroke-width", "1.25")
+        .attr("x", "202")
+        .attr("height", 13)
+        .attr("width", function(d) {
+            console.log(d)
+            return d.values.length / 3;
+            //return xScale(d.values);
+        })
+        .attr("y", function(d, i) { 
+            return i * 28 + 90; 
+        });
 
+    //Review mouse over label
     d3.selectAll("rect")
-    .on("hover", function(d) {
-        console.log(d);
-    })
-    .on("mousemove", function(d) {
-        var mouse = d3.mouse(document.body);
-        d3.select("#tooltip")
-            .style("display", "block")
-            .html("<h1>" + d + "</h1>")
-            .style("left", mouse[0] + "px")
-            .style("top", mouse[1] - 50 + "px");
-    })
-    .on("mouseout", function(d) {
-        d3.select("#tooltip")
-            .style("display", "none")
-    });
+        .on("mouseover", function(d) {
+            var mouse = d3.mouse(document.body);
+            d3.select("#tooltip")
+                .style("display", "block")
+                .html("<h4>" + d.values + "</h4>")
+                .style("left", mouse[0] + "px")
+                .style("top", mouse[1] - 50 + "px");
+        })
+        .on("mouseout", function(d) {
+            d3.select("#tooltip")
+                .style("display", "none")
+        });
 
-    rectIssueFrequency.filter(function(d) { return d.values == null; }).remove();
-
+    //Review empty issue exclude
+    rectIssueFrequency
+        .filter(function(d) { 
+            return d.key == null; 
+        }).remove();
+        
     var issueMapping = {
         "1": "Criminal Procedure",
         "2": "Civil Rights",
@@ -61,20 +106,19 @@ d3.csv("scotusdatabyissue2.16.19.csv", function(error, data) {
         "11": "Interstate Relation",
         "12": "Federal Taxation",
         "13": "Other",
-        "14": "Private Law"
+        "14": "Private Law",
         };
           
     groupIssue.forEach(function(d) {
         d.issue = issueMapping[d.key];
         });
 
-    
-        var labelIssueEnter = svg.selectAll("text")
+    var labelIssueEnter = svg.selectAll("text")
         .data(groupIssue)
         .enter()
         .append("text");
 
-        labelIssueEnter
+    labelIssueEnter
         .attr("fill", "darkslategrey")
         .attr("font-family", "opensans")
         .attr("font-size", "14px")
@@ -87,6 +131,9 @@ d3.csv("scotusdatabyissue2.16.19.csv", function(error, data) {
         })
         .attr("dy", "10px");
     });
+
+
+
 
 
 
